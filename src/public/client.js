14072b19234c7e3cc8c9671bd4e39a308b5b066f
Piroska Werner <piroska.werner@gmail.com>
2020-12-04 09:34:15 -0600

    feat: Fetch rover manifest data

--- src/public/client.js
+++ src/public/client.js
@@ -1,6 +1,11 @@
 let store = {
     user: { name: "Student" },
     apod: '',
+    photos: '',
+    roverShown: 'Curiosity',
+    curiosity: '',
+    opportunity: '',
+    spirit: '',
     rovers: ['Curiosity', 'Opportunity', 'Spirit'],
 }
 
@@ -8,7 +13,11 @@ let store = {
 const root = document.getElementById('root')
 
 const updateStore = (store, newState) => {
+    // console.log(`Old state:\n ${store.photos}`)
+    console.log(`updating store with new state`)
+    console.log(newState);
     store = Object.assign(store, newState)
+    // console.log(`Updated state:\n${store.photos}`)
     render(root, store)
 }
 
@@ -19,14 +28,17 @@ const render = async (root, state) => {
 
 // create content
 const App = (state) => {
-    let { rovers, apod } = state
+    let { rovers, roverShown, roverData, curiosity, apod, photos } = state
+    // console.log(rovers)
+
 
     return `
-        <header></header>
+        <header><h3>Choose Your Rover</h3></header>
         <main>
-            ${Greeting(store.user.name)}
+            ${Rovers(rovers, roverShown)}
+
             <section>
-                <h3>Put things on the page!</h3>
+                ${AboutRover(roverShown, curiosity)}
                 <p>Here is an example section.</p>
                 <p>
                     One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
@@ -37,7 +49,9 @@ const App = (state) => {
                     but generally help with discoverability of relevant imagery.
                 </p>
                 ${ImageOfTheDay(apod)}
-            </section>
+                ${rovers}
+                ${photosOfMars(photos)}
++            </section>
         </main>
         <footer></footer>
     `
@@ -61,6 +75,41 @@ const Greeting = (name) => {
     return `
         <h1>Hello!</h1>
     `
+}
+
+const Rovers = (rovers, roverShown) => {
+    // console.log(`When making radio buttons roverShown: ${roverShown}`)
+    let radioButtons = ''
+    rovers.map(rover => {
+        let radioButton = `<input type="radio" id="roverButtons" name="${rover}" value="${rover}"`
+        if (rover === roverShown) {
+            radioButton += 'checked'
+        }
+        radioButton += `>${rover}`
+        radioButtons += radioButton
+    })
+    return radioButtons
+}
+
+const AboutRover = (roverShown, curiosity) => {
+    if (!curiosity) {
+        getRoverData(store)
+    }
+
+    let photos = curiosity.roverData.photo_manifest.photos
+    console.log(`Total # of photos available: ${photos.length}`)
+    const latestPhotoDate = curiosity.roverData.photo_manifest.max_date
+    let latestPhotos = photos.filter(photo => photo.earth_date === latestPhotoDate)
+    console.log(`Number of latest day photos: ${latestPhotos.length}`)
+
+    return `
+        <h2>About ${roverShown}</h2>
+        <p>Launch date: ${curiosity.roverData.photo_manifest.launch_date}</p>
+        <p>Landing date: ${curiosity.roverData.photo_manifest.landing_date}</p>
+        <p>Status: ${curiosity.roverData.photo_manifest.status}</p>
+        <p>Date of latest photos available: ${curiosity.roverData.photo_manifest.max_date}</p>
+    `
+        // <p>${curiosity.rover.photo_manifest.name}</p>
 }
 
 // Example of a pure function that renders infomation requested from the backend
@@ -91,6 +140,20 @@ const ImageOfTheDay = (apod) => {
     }
 }
 
+const photosOfMars = (photos) => {
+    // console.log(`Photos when entering marsPhotos: ${photos}`)
+    if (!photos) {
+        // console.log(`photos where empty calling getMarsPhotos`)
+        getMarsPhotos(store)
+    }
+    // console.log(`Photos after getting mars pohtos: ${photos.photos.photos}`);
+    return (`
+        <p>Here are the mars photos</p>
+        <p>${photos}</p>
+        <p>${photos.photos.photos[0].id}</p>
+    `)
+}
+
 // ------------------------------------------------------  API CALLS
 
 // Example API call
@@ -101,5 +164,26 @@ const getImageOfTheDay = (state) => {
         .then(res => res.json())
         .then(apod => updateStore(store, { apod }))
 
-    return data
+    // return data
+}
+
+const getMarsPhotos = (state) => {
+    let { photos } = state
+    console.log(`After setting phtots by state: ${photos}`)
+
+    fetch(`http://localhost:3000/marsPhotos`)
+        .then(res => {
+            console.log(`response from marsPhotos: ${res}`)
+            return res.json()})
+        .then(photos => updateStore(store, { photos }))
+}
+
+
+const getRoverData = (state) => {
+    const { roverShown } = state
+
+    // fetch(`http://localhost:3000/${roverShown}`)
+    fetch(`http://localhost:3000/curiosity`)
+        .then(res => res.json())
+        .then(curiosity => updateStore(store, { curiosity }))
 }
--- src/server/index.js
+++ src/server/index.js
@@ -17,12 +17,39 @@ app.use('/', express.static(path.join(__dirname, '../public')))
 // example API call
 app.get('/apod', async (req, res) => {
     try {
-        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
+        // let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
+        // let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=sUHsXIwebQ2Yztzj2DILp5goJaMM5QQBWhhp3Q7v`)
             .then(res => res.json())
         res.send({ image })
+        // .then(res => console.log(res))
     } catch (err) {
         console.log('error:', err);
     }
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.get('/marsPhotos', async (req, res) => {
+	try {
+		console.log('trying to get mars photos')
+		let photos = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=sUHsXIwebQ2Yztzj2DILp5goJaMM5QQBWhhp3Q7v`)
+		.then(res => res.json())
+		// 	console.log(res.json())
+		// 	return res.json()})
+		res.send({ photos })
+		// .then(data => console.log(data))
+	} catch (err) {
+		console.log('error:', err)
+	}
+})
+
+app.get('/curiosity', async (req, res) => {
+	try {
+		let roverData = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/curiosity/?api_key=sUHsXIwebQ2Yztzj2DILp5goJaMM5QQBWhhp3Q7v`)
+		.then(res => res.json())
+		res.send({ roverData })
+	} catch (err) {
+		console.log('error:', err)
+	}
+})
